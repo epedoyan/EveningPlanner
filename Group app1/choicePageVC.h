d6dcc57ae7762a5dbc007c84bc30ce197bba1f1a
Emma Pedoyan <epedoyan@gmail.com>
2016-04-07 01:54:23 +0400

    Updates on users choice VC

---
+++ Group app1/choicePageVC.h
@@ -0,0 +1,13 @@
+//
+//  choicePageVC.h
+//  Group app1
+//
+//  Created by User on 4/7/16.
+//  Copyright © 2016 Paruyr Danielian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface choicePageVC : UIViewController
+
+@end
---
+++ Group app1/choicePageVC.m
@@ -0,0 +1,32 @@
+//
+//  choicePageVC.m
+//  Group app1
+//
+//  Created by User on 4/7/16.
+//  Copyright © 2016 Paruyr Danielian. All rights reserved.
+//
+
+#import "choicePageVC.h"
+
+@interface  choicePageVC () <UITableViewDataSource>
+
+@end
+
+@implementation choicePageVC
+
+- (void)viewDidLoad {
+    	[super viewDidLoad];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 4;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"choiceCell"
+                                                            forIndexPath:indexPath];
+    return cell;
+}
+
+@end
