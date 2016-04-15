055d8c6bd37a2a573b2f4a5575bd9547d8c47c28
Musho <musho@example.com>
2016-04-16 01:41:23 +0300

    Map and Web ViewControllers updated. SecondViewController improved.

--- Group app1/WebViewController.m
+++ Group app1/WebViewController.m
@@ -18,7 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSURL *url = [NSURL URLWithString:@"https://www.kfc.com/"];
+    NSURL *url = [NSURL URLWithString:self.urlString];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     
     [self.webView loadRequest:request];
